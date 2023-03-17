37c486152f5be0185f39b21457e5dc0e3381f6e1
Марат Музафаров <mmuzafarov74@gmail.com>
2023-03-17 09:45:27 +0500

    бд

---
+++ latuc/Data/Model/Level.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace latuc;
+
+public partial class Level
+{
+    public int Idlevels { get; set; }
+
+    public int Answer { get; set; }
+
+    public string AnswerPractic { get; set; } = null!;
+
+    public string Question { get; set; } = null!;
+}
---
+++ latuc/Data/Model/UserAchievement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace latuc;
+
+public partial class UserAchievement
+{
+    public int IduserAchievements { get; set; }
+
+    public int Taken { get; set; }
+
+    public int IdUser { get; set; }
+
+    public int IdAchievements { get; set; }
+
+    public virtual Achievement IdAchievementsNavigation { get; set; } = null!;
+
+    public virtual ICollection<User> Users { get; } = new List<User>();
+}
