9c5280764aa1801b07fec3a29f3bdd2daa453e86
Марат Музафаров <mmuzafarov74@gmail.com>
2023-03-31 12:09:52 +0500

    new db

---
+++ latuc/Data/Model/UserRole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace latuc.Data.Model;
+
+public partial class UserRole
+{
+    public int IduserRole { get; set; }
+
+    public string Role { get; set; } = null!;
+
+    public virtual ICollection<User> Users { get; } = new List<User>();
+}
