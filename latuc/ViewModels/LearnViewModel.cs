97a1da252292d47420ecba17fb901bba17da0ebf
user_3_19P1 <user_3_19p1@mail.ru>
2023-10-24 13:49:37 +0500

    add rating

--- latuc/ViewModels/LearnViewModel.cs
+++ latuc/ViewModels/LearnViewModel.cs
@@ -13,7 +13,36 @@ namespace latuc.ViewModels
         private readonly UserService _userService;
         private readonly PageService _pageService;
         private readonly LevelsService _levelsService;
-        public int RatingTheory { get; set; }
+
+        public int RatingTheoryStructure { get; set; }
+        public int RatingTheoryPeremen { get; set; }
+        public int RatingTheoryType { get; set; }
+        public int RatingTheoryConstruct { get; set; }
+        public int RatingTheoryCicle { get; set; }
+        public int RatingTheoryMassive { get; set; }
+        public int RatingTheoryReturn { get; set; }
+        public int RatingTheorySwitch { get; set; }
+        public int RatingTheoryRecursive { get; set; }
+
+        public int RatingTestStructure { get; set; }
+        public int RatingTestPeremen { get; set; }
+        public int RatingTestType { get; set; }
+        public int RatingTestConstruct { get; set; }
+        public int RatingTestCicle { get; set; }
+        public int RatingTestMassive { get; set; }
+        public int RatingTestReturn { get; set; }
+        public int RatingTestSwitch { get; set; }
+        public int RatingTestRecursive { get; set; }
+
+        public int RatingPracticStructure { get; set; }
+        public int RatingPracticPeremen { get; set; }
+        public int RatingPracticType { get; set; }
+        public int RatingPracticConstruct { get; set; }
+        public int RatingPracticCicle { get; set; }
+        public int RatingPracticMassive { get; set; }
+        public int RatingPracticReturn { get; set; }
+        public int RatingPracticSwitch { get; set; }
+        public int RatingPracticRecursive { get; set; }
 
         public DelegateCommand<string> Theory { get; set; }
 
@@ -27,7 +56,85 @@ namespace latuc.ViewModels
             _pageService = pageService;
             _levelsService = levelsService;
 
-                RatingTheory = 1;
+            List<LevelsStatistic> levels = _levelsService.getLevelRating();
+
+            if (levels != null)
+            {
+                foreach (var item in levels)
+                {
+                    switch (item.Idlevels.ToString())
+                    {
+                        case ("0"):
+                            {
+                                RatingTheoryStructure = item.ScoreTheory;
+                                RatingTestStructure = item.ScoreTest;
+                                RatingPracticStructure = item.ScorePractic;
+                            }
+                            break;
+                        case ("1"):
+                            {
+                                RatingTheoryPeremen = item.ScoreTheory;
+                                RatingTestPeremen = item.ScoreTest;
+                                RatingPracticPeremen = item.ScorePractic;
+                            }
+                            break;
+                        case ("2"):
+                            {
+                                RatingTheoryType = item.ScoreTheory;
+                                RatingTestType = item.ScoreTest;
+                                RatingPracticType = item.ScorePractic;
+                            }
+                            break;
+                        case ("3"):
+                            {
+                                RatingTheoryConstruct = item.ScoreTheory;
+                                RatingTestConstruct = item.ScoreTest;
+                                RatingPracticConstruct = item.ScorePractic;
+                            }
+                            break;
+                        case ("4"):
+                            {
+                                RatingTheoryCicle = item.ScoreTheory;
+                                RatingTestCicle = item.ScoreTest;
+                                RatingPracticCicle = item.ScorePractic;
+                            }
+                            break;
+                        case ("5"):
+                            {
+                                RatingTheoryMassive = item.ScoreTheory;
+                                RatingTestMassive = item.ScoreTest;
+                                RatingPracticMassive = item.ScorePractic;
+                            }
+                            break;
+                        case ("6"):
+                            {
+                                RatingTheoryReturn = item.ScoreTheory;
+                                RatingTestReturn = item.ScoreTest;
+                                RatingPracticReturn = item.ScorePractic;
+                            }
+                            break;
+                        case ("7"):
+                            {
+                                RatingTheorySwitch = item.ScoreTheory;
+                                RatingTestSwitch = item.ScoreTest;
+                                RatingPracticSwitch = item.ScorePractic;
+                            }
+                            break;
+                        case ("8"):
+                            {
+                                RatingTheoryRecursive = item.ScoreTheory;
+                                RatingTestRecursive = item.ScoreTest;
+                                RatingPracticRecursive = item.ScorePractic;
+                            }
+                            break;
+                       
+
+                    }
+                   
+                    
+                }
+            }
+            
 
             Theory = new DelegateCommand<string>(TheoryMethod);
             Test = new DelegateCommand<string>(TestMethod);
